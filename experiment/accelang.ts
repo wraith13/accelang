c95dfca32d9498d3af5a8983ff41b112b83c9e94
道化師 <wraith@trickpalace.net>
2017-11-04 18:52:47 +0900

    🛠 試験的な仮実装 ( WIP )

--- experiment/accelang.ts
+++ experiment/accelang.ts
@@ -1,1 +1,1 @@
-document.getElementById("body").getElementsByClassName("container")[0].innerHTML = "TypeScript worked.";
+//document.getElementById("body").getElementsByClassName("container")[0].innerHTML = "TypeScript worked.";
---
+++ experiment/experiment.ts
@@ -0,0 +1,36 @@
+//  copy from https://stackoverflow.com/questions/36532307/rem-px-in-javascript
+function convertRemToPixels(rem)
+{
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+setTimeout
+(
+    () =>
+    {
+        document.getElementsByClassName("sample-list")[0].getElementsByClassName("container")[0].getElementsByTagName("ul")[0].innerHTML = "<li>item</li>";
+        fill_height();
+        window.onresize = fill_height;
+    },
+    10
+);
+
+function fill_height() : void
+{
+    const list = [
+        document.getElementsByTagName("textarea")[0],
+        //  なぜか Cast の構文がインタプリタで利用できない。。。
+        //<HTMLElement>document.getElementsByClassName("xxx")[0]
+        document.getElementsByClassName("xxx")[0]
+    ];
+    if (convertRemToPixels(80) <= document.body.clientWidth)
+    {
+        list.forEach(element => element.style.width = "100%");
+        list.forEach(element => element.style.height = (document.body.clientHeight -convertRemToPixels(7)) +"px");
+    }
+    else
+    {
+        list.forEach(element => element.style.width = (document.body.clientWidth -convertRemToPixels(1)) +"px");
+        list.forEach(element => element.style.height = "30vh");
+    }
+}
