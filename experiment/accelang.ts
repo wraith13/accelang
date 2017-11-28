17d574393d5f640af0a6f61ff73a76462e92de62
道化師 <wraith@trickpalace.net>
2017-11-29 02:51:24 +0900

    🛠 試験的な仮実装 ( WIP )

--- experiment/accelang.ts
+++ experiment/accelang.ts
@@ -14,13 +14,22 @@ module accelang
         };
     }
     
-    export function evaluate(code : object) : any
+//    export function call(code : object, context : object) : any
+//    {
+//        const target = evaluate(code["target"], context);
+//        const argument = evaluate(code["argument"], context);
+//
+//        
+//    }
+
+    export function load(code : object) : any
     {
-        if (null === code)
-        {
-            return null;
-        }
-        const type = code["&a"];
+        //  この関数の役割は全てのコードおよびデータをコードからアクセス可能な状態にすること。
+        //  シンタックスエラーの類いの検出はこの関数内で行ってしまう。
+        //  エラーを検出しても可能な限り最後まで処理を行う。
+        //  この関数では例えそれが定数であっても評価は一切しない。(というかこの関数の処理が完了するまでは定数を評価する為に必要なコードやデータがアクセス可能な状態になっている保証がない。)
+
+        const type = code["&A"];
         if (undefined === type || null === type)
         {
             return make_error("format error(missing type)");
@@ -32,6 +41,51 @@ module accelang
             case "string":
                 switch(type)
                 {
+//                case "call":
+//                    return call(code, context);
+
+                default:
+                    return make_error(`uknown type error: ${type}`);
+                }
+                
+            case "object":
+                {
+                    //const complex_type = accelang.evaluate(type);
+                    break;
+                }
+
+            default:
+                return make_error(`format error(invalid type): ${typeof(type)}, ${JSON.stringify(type)}`);
+            }
+        }
+        return make_error("intenal error");
+    }
+    
+    export function evaluate(code : object, context : object = null) : any
+    {
+        if (null === code)
+        {
+            return null;
+        }
+        if (null === context)
+        {
+            context = load(code);
+        }
+        const type = code["&A"];
+        if (undefined === type || null === type)
+        {
+            return make_error("format error(missing type)");
+        }
+        else
+        {
+            switch(typeof(type))
+            {
+            case "string":
+                switch(type)
+                {
+//                case "call":
+//                    return call(code, context);
+
                 default:
                     return make_error(`uknown type error: ${type}`);
                 }
